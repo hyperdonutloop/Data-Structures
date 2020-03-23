2563cff5d47af42ad1e6da1289964e651d5dff5f
donutwizard666 <thebeardedwarrior@icloud.com>
2020-03-23 10:55:50 -0700

    fixed colon error

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -108,7 +108,7 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        if node is self.tail
+        if node is self.tail:
             return
         value = node.value
         self.delete(node)
